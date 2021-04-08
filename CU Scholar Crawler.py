1f2d26606935092e37216060638f6682d03761c4
Habeeb Adelase <tola_adelase@yahoo.com>
2021-04-08 03:04:00 +0100

    Update CU Scholar Crawler.py
    This one is working a bit now.

--- CU Scholar Crawler.py
+++ CU Scholar Crawler.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
+#webbrowser.open(url)
 
 
 def mycrawler(seed, maxcount):
@@ -11,26 +13,35 @@ def mycrawler(seed, maxcount):
         Q = Q[1:]
         print(":" + url)
 
+        # def fetch(url):
+        # try:
         code = requests.get(url)
-        plain = code.text
-        s = BeautifulSoup(plain, "html.parser")
-        for link in s.findAll('a', {'class': 'gs_ai_pho'}):
-            new_url = link.get('href')
-            if new_url is not None and new_url != '/':
-                new_url = new_url.strip()
-                # print("new_url is : ", new_url)
+    # except requests.exceptions.ConnectionError:
+    # print('Given URL: "%s" is not available!' % url)
+    # return
 
-                # normalise if needed
-                if new_url[0:7] != 'http://' and new_url[0:8] != 'https://':
-                    if url[len(url) - 1] == '/':
-                        new_url = url + new_url
-                    else:
-                        new_url = url + '/' + new_url
-                        # NOTE : further normalization code required here
-                # print("new_url is : ", new_url)  #uncomment the print statement to see the urls
+    plain = code.text
+    s = BeautifulSoup(plain, "html.parser")
+    for link in s.findAll('a', {'class': 'gs_ai_pho'}):
+        new_url = link.get('href')
+        if new_url is not None and new_url != '/':
+            new_url = new_url.strip()
+            # print("new_url is : ", new_url)
 
-                Q.append(new_url)
+            # normalise if needed
+            if new_url[0:7] != 'https://' and new_url[0:8] != 'https://':
+                if url[len(url):0] == '/':
+                    new_url = 'https:' + url.strip(
+                        '/citations?view_op=view_org&hl=en&org=9117984065169182779') + new_url
+                else:
+                    new_url = 'https' + url.strip(
+                        '/citations?view_op=view_org&hl=en&org=9117984065169182779') + new_url
+
+                    print("new_url is : ", new_url)  # uncomment the print statement to see the urls
+                    Q.append(new_url)
 
 
 # use any number, instead of 10, to control the number of pages crawled
-mycrawler('https://scholar.google.co.uk/citations?view_op=view_org&hl=en&org=9117984065169182779', 20)
+mycrawler('https://scholar.google.co.uk/citations?view_op=view_org&hl=en&org=9117984065169182779', 15)
+
+# fetch(mycrawler)
