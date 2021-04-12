ca8c36088c8f2da2191caa320ee8718e494f3b8e
Habeeb Adelase <tola_adelase@yahoo.com>
2021-04-12 18:06:44 +0100

    CU crawler

--- CU Crawler.py
+++ CU Crawler.py
@@ -1,36 +1,61 @@
 import json
+import re
+
 import requests
 from bs4 import BeautifulSoup
+import csv
+import time
+
+headers = {
+    'User-Agent': 'Adetola Adelase',
+    'From': 'adelasea@uni.coventry.ac.uk'
+}
+
+f = csv.writer(open('No.csv', 'w'))
+f.writerow(['Name', 'Profile'])
 
 start_url = 'https://scholar.google.co.uk/citations?view_op=view_org&hl=en&org=9117984065169182779'
+response = requests.get(start_url, headers=headers)
+content = BeautifulSoup(response.text, 'lxml')
 
+page_count_links = content.findAll('button',
+                                   {'type': 'button'})
+num_pages = str(page_count_links[1]).split(" ")[8].split("=")[2]
+print(str(page_count_links[1]).split(" ")[8].split("=")[2])
+try:  # Make sure there are more than one page, otherwise, set to 1.
+    num_pages = str(page_count_links[1]).split(" ")[8].split("=")[2]
+except IndexError:
+    num_pages = 1
 
-def fetch(url):
-    try:
-        response = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        print('Given URL: "%s" is not available!' % url)
-        return
+url_list = ["{}+{}".format(start_url, num_pages)]
+new_url = 'https://scholar.google.co.uk' + num_pages.replace("'", "")
+final_url = new_url.replace("\\","/")
+print(final_url)
 
-    content = BeautifulSoup(response.text, 'lxml')
+name = content.findAll('h3', {'class': 'gs_ai_name'})
+links = content.findAll('a', {'class': 'gs_ai_pho'})
+dept = content.findAll('div', {'class': 'gs_ai_aff'})
+title = content.findAll('div', {'class': 'gs_ai_int'})
+papers = content.findAll('a', {'class': 'gsc_a_at'})
+names_on_papers = content.findAll('div', {'class': 'gs_gray'})
 
-    links = content.findAll('a', {'class': 'gs_ai_pho'})
-    name = content.findAll('h3', {'class': 'gs_ai_name'})
-    dept = content.findAll('div', {'class': 'gs_ai_aff'})
-    cited = content.findAll('div', {'class': 'gs_ai_cby'})
-    title = content.findAll('div', {'class': 'gs_ai_int'})
+print(len(links), len(name))
 
-    print(len(links), len(name))
-    for index in range(0, len(name)):
-        page = {
-            'name': name[index].text.strip().replace('\n', ''),
-            'dept': dept[index].text.strip().replace('\n', ''),
-            'title': title[index].text,
-            'cited': cited[index].text.strip().replace('\n', ''),
-            'url': links[index]['href'],
+for index in range(0, len(links)):
+    page = {
+        'name': name[index].text.strip().replace('\n', ''),
+        'url': 'https://scholar.google.co.uk' + links[index]['href'],
+        'dept': dept[index].text.strip().replace('\n', ''),
+        'title': title[index].text.replace(' ', ', '),
+        # 'papers': start_url + papers[index]['data-href'].strip().replace('\n', ''),
+        # 'names_on_papers': names_on_papers[index].text.strip().replace('\n', ''),
 
-        }
-        print(json.dumps(page, indent=2))
+    }
 
+    print(json.dumps(page, indent=4))
 
-fetch(start_url)
+for index in range(0, len(name)):
+    names = name[index].text.strip().replace('\n', '')
+    profile = 'https://scholar.google.co.uk' + links[index]['href']
+
+    # f.writerow([names, profile])
